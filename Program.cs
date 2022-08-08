8c6c14c1dd51d3dceb6cf16ebfeba3aac1b1667b
Alexey Platonov <alex_platonov1@mail.ru>
2022-08-08 21:25:05 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Net;
+
+namespace SIP_log
+{
+    class Program
+    {
+        public static bool DEBUG_MODE = false;
+        static void Main()
+        {
+            send_ip();
+        }
+        static int send_ip()
+        {
+            try
+            {
+                WebRequest request = WebRequest.Create("http://127.0.0.1");
+                request.Credentials = CredentialCache.DefaultCredentials;
+                WebResponse response = request.GetResponse();
+                response.Close();
+                return 0;
+            }
+            catch
+            {
+                return 1;
+            }
+        }
+    }
+}
